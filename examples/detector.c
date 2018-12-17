6b7170ee5ff65d1783126d7ad76ee1365b0a8647
RyanRL <31579892+RyanRL@users.noreply.github.com>
2018-12-16 21:15:34 -0500

    Update detector.c

--- examples/detector.c
+++ examples/detector.c
@@ -591,9 +591,9 @@ void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filenam
         //resize_network(net, sized.w, sized.h);
         layer l = net->layers[net->n-1];
         
-        printf("%d %d %d.\n",net.h, net.w, net.c);
-        printf("%f.\n",net.outputs);
-        printf("%d.\n",l.n);
+        printf("%d %d %d.\n",net->h, net->w, net->c);
+        printf("%f.\n",net->outputs);
+        printf("%d.\n",l->n);
         printf("%d.\n",net->n);
         float *X = sized.data;
         time=what_time_is_it_now();
